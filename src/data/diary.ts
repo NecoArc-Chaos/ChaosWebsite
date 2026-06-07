7356123f0da52e63a2ae3ba40d2f6b07c3587c1c
Neco-ArcChaos <acceleratoor0123@gmail.com>
2026-06-07 22:18:19 +0800

    fix: 更新日记内容，添加memos日记入口链接

--- src/data/diary.ts
+++ src/data/diary.ts
@@ -16,7 +16,7 @@ const diaryData: DiaryItem[] = [
 	{
 		id: 1,
 		content:
-			"The falling speed of cherry blossoms is five centimeters per second!",
+			"memos日记入口：https://memos.necoarcchaos.xyz:45678/ \n\n或者 183.66.27.19:45678",
 		date: "2025-01-15T10:30:00Z",
 		images: ["/images/diary/sakura.jpg", "/images/diary/1.webp"],
 	},
